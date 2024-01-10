696e7cffa5349e4257d15139022915d92793ee18
Jose Carlos Juarez <josecarlosjc30@gmail.com>
2024-01-10 23:50:06 +0100

    Analitics #10/01/2023

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <SpeedInsights />
+    <Analytics />
     <App />
   </React.StrictMode>
 );
